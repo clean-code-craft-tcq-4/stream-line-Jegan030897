8045fee154acbb3c53c7c0181ed3de5ccb830130
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-08 20:38:17 +0530

    Update Generate_BMS_sensorData.h

--- Generate_BMS_sensorData.h
+++ Generate_BMS_sensorData.h
@@ -1,4 +1,10 @@
 #include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define MAX_DATA        50
 #define TEMP_RANGE      45
@@ -8,8 +14,29 @@
 #define MIN_SOC		      20
 #define MAX_SOC		      80
 
+#define TRUE            1
+#define FALSE           0
+
+typedef enum 
+{
+  FILE_READ    = 0,
+  FILE_WRITE   = 1
+}fileDirectory;
+
+typedef enum 
+{
+  BATTERY_TEMPERATURE    = 0,
+  BATTERY_SOC            = 1
+}Parameter;
+
+Parameter BatteryParameter; 
+
 int BatteryTemp[MAX_DATA];
-int Soc[MAX_DATA];
+int BatterySoc[MAX_DATA];
+
+int Temp_fileDirectory[2]; 
+int SOC_fileDirectory[2]; 
+int id;
 
 void Generate_TempSensorData(int *batTemp, int Datasize);
 void Generate_SOCData(int *socData, int Datasize);
