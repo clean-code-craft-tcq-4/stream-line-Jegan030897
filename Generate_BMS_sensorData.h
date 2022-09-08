aabce3b64a9377744ba25e051c2c934d28e6c455
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-08 19:32:05 +0530

    Create Generate_BMS_sensorData.h

---
+++ Generate_BMS_sensorData.h
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+#define MAX_DATA        50
+#define TEMP_RANGE      45
+#define SOC_RANGE       80
+#define MIN_TEMP	      0
+#define MAX_TEMP	      45
+#define MIN_SOC		      20
+#define MAX_SOC		      80
+
+int BatteryTemp[MAX_DATA];
+int Soc[MAX_DATA];
+
+int Generate_TempSensorData(int Datasize);
+int Generate_SOCData(int Datasize);  
