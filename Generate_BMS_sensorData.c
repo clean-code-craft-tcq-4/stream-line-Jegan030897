eb8cd3d4e7d93fe5ed98e02df2d2bf8e9c89f055
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-08 19:54:12 +0530

    Update Generate_BMS_sensorData.c

--- Generate_BMS_sensorData.c
+++ Generate_BMS_sensorData.c
@@ -2,32 +2,32 @@
 #include "stdlib.h"
 #include "Generate_BMS_sensorData.h"
 
-void Generate_TempSensorData(int Datasize)
+void Generate_TempSensorData(int *batTemp, int Datasize)
 {
   for(int index = 0; index < Datasize; index++)
   {
-		int randData = rand()%TEMP_RANGE;
-		if(randData > MIN_TEMP && randData < MAX_TEMP)
-		{
-			BatteryTemp[index] = randData;
-		}
+	int randData = rand()%TEMP_RANGE;
+	if(randData > MIN_TEMP && randData < MAX_TEMP)
+	{
+		*(batTemp+index) = randData;
+	}
   }
 }
 
-void Generate_SOCData(int Datasize)
+void Generate_SOCData(int *socData, int Datasize)
 {
   for(int index = 0; index < Datasize; index++)
   {
-		int randData = rand()%SOC_RANGE;
-		if(randData > MIN_SOC && randData < MAX_SOC)
-		{
-			Soc[index] = randData;
-		}
+	int randData = rand()%SOC_RANGE;
+	if(randData > MIN_SOC && randData < MAX_SOC)
+	{
+		*(socData+index) = randData;
+	}
   } 
 }
 
 int main()
 {
-	Generate_TempSensorData(MAX_DATA);
-	Generate_SOCData(MAX_DATA);
+	Generate_TempSensorData(BatteryTemp, MAX_DATA);
+	Generate_SOCData(Soc, MAX_DATA);
 }
