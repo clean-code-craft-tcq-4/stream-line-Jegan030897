89c7bdfa9438ce61d8a278997417cab360a56cad
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-09 10:59:52 +0530

    Update Generate_BMS_sensorData.h

--- Generate_BMS_sensorData.h
+++ Generate_BMS_sensorData.h
@@ -51,7 +51,6 @@ void Generate_SOCData(int *socData, int Datasize);
 int settingPipeforDataTransition(int *tempFD, int *socFD);
 int serialCommunication(int *receiveBatTemp, int *receiveSocData);
 int GenerateSensorData_Tx(int *BatteryTemp, int *BatterySoc, int tempDataLen, int socDataLen);
-void serialCom_TxData(char dataArray[]);
-void serialCom_RxData();
+
 
 
