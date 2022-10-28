c96db9038801f5239848f2d3faeeeadcfe10ce70
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-10-28 16:47:27 +0530

    Update Test_Receiver_BMSStatistics.c

--- Test_Receiver_BMSStatistics.c
+++ Test_Receiver_BMSStatistics.c
@@ -1,6 +1,6 @@
 #include <assert.h>
-#include "Sender/Generate_BMS_Data.h"
-#include "Receiver/Rx_BMS_Data.h"
+#include "Generate_BMS_Data.h"
+#include "./Receiver/Rx_BMS_Data.h"
 #include "BMS_Statistics/BMS_Statistics.h"
 
 void Test_ReadMock_BMSdata();
